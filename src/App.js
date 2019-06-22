23733ed27e17ec6dcf21dfd7dfa9b7af103b0a69
Greg Foreman <gfore@mindspring.com>
2019-06-22 12:51:09 -0400

    adds header with AppBar, ToolBar, and Typography

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <AppBar color="primary" position="static">
-        <h1>My Header</h1>
+        <ToolBar>
+          <Typography variant="h4" component="h1" color="inherit">
+            My Header
+          </Typography>
+        </ToolBar>
       </AppBar>
     </div>
   );
