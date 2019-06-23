d06948a6c321f49e583dd2fc18e451eb6e5c1c61
Greg Foreman <gfore@mindspring.com>
2019-06-23 13:56:28 -0400

    adds icons to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { List, ListItem, ListItemText, Typography } from '@material-ui/core/';
+import { List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core/';
+import { AccountBox, Book, Home } from '@material-ui/icons/';
+
+const MenuItems = ({ menuList }) => {
+  return menuList.map((menuItem, i) => {
+    return (
+      <ListItemText inset key={i}>
+        <Typography color="inherit" variant="h5">
+          {menuItem === 'Home' ? 
+              <Home />
+            : menuItem === 'Posts' ?
+              <Book />
+            : <AccountBox />
+          } {`${menuItem}`}
+        </Typography>
+      </ListItemText>
+    );
+  });
+}
 
 function Navbar(props) {
   return (
     <List component="nav">
       <ListItem component="div">
-          <ListItemText inset><Typography color="inherit" variant="h5">Home</Typography></ListItemText>
-          <ListItemText inset><Typography color="inherit" variant="h5">Posts</Typography></ListItemText>
-          <ListItemText inset><Typography color="inherit" variant="h5">Contact</Typography></ListItemText>
+        <MenuItems menuList={['Home', 'Posts', 'Contact']} />
       </ListItem>
     </List>
   );
